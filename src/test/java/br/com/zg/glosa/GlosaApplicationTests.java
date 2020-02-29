8b57674fac9e006d29888b24d215d32ed8cf910e
Pedro Ribeiro <phsony26@gmail.com>
2020-02-29 14:21:16 -0300

    exemplo para salvar o arquivo da internet

--- src/test/java/br/com/zg/glosa/GlosaApplicationTests.java
+++ src/test/java/br/com/zg/glosa/GlosaApplicationTests.java
@@ -1,13 +1,42 @@
 package br.com.zg.glosa;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GlosaApplicationTests {
+public class GlosaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+   @Test
+   public void contextLoads() {
+      try {
+         downloadAR();
+      } catch (Exception e) {
+         // TODO: handle exception
+      }
+   }
+
+   private void downloadAR() throws IOException {
+      URL url = new URL(
+            "https://glosamax.zeroglosa.com.br/glosamax/arquivo/download?nome=glosamax_2018-02-15.xml&data=2018-02-15");
+      String destino = "catalog_java.xml";
+
+      InputStream is = url.openStream();
+      FileOutputStream fos = new FileOutputStream(destino);
+
+      int bytes = 0;
+
+      while ((bytes = is.read()) != -1) {
+         fos.write(bytes);
+      }
+
+      is.close();
+
+      fos.close();
+   }
 
 }

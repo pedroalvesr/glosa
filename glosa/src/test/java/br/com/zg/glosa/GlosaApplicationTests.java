c53acea920dc9de8064cadef1c64863e0bf5cfb8
Pedro Ribeiro <phsony26@gmail.com>
2020-02-29 13:58:02 -0300

    primeiro commit

---
+++ glosa/src/test/java/br/com/zg/glosa/GlosaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zg.glosa;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
